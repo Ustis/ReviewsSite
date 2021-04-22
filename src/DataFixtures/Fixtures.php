b9003d7edf82df14cf50b27f3e79ed24a5166132
Ustis <mihail.2000@bk.ru>
2021-04-22 15:34:45 +0300

    _

--- src/DataFixtures/Fixtures.php
+++ src/DataFixtures/Fixtures.php
@@ -5,28 +5,50 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use function Sodium\add;
 
-class Fixtures extends Fixture
+class Fixtures extends Fixture implements ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
-    public function load(ObjectManager $manager)
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load($manager)
     {
         // $product = new Product();
         // $manager->persist($product);
 
-//        $user = new User();
-//        UserPasswordEncoder $passwordEncoder = new UserPasswordEncoder();
-//
-//        $user->setEmail('user@mail.com');
-//        $user->setPassword($this->passwordEncoder->encodePassword('user'));
-//        $user->setRoles('ROLE_USER');
-//
-//        $admin = new User();
-//
-//        $admin->setEmail('admin@mail.com');
-//        $admin->setPassword($this->passwordEncoder->encodePassword('admin'));
-//        $admin->setRoles('ROLE_ADMIN');
+        $user = new User();
+
+        $user->setEmail('user@mail.com');
+        $hashUser = $this->container->get('security.password_encoder')->encodePassword($user,'user');
+        $user->setPassword($hashUser);
+        $rolesUser = array();
+        array_push($rolesUser, 'ROLE_USER');
+        $user->setRoles($rolesUser);
+
+
+        $admin = new User();
+
+        $admin->setEmail('admin@mail.com');
+        $hashAdmin = $this->container->get('security.password_encoder')->encodePassword($user,'admin');
+        $admin->setPassword($hashAdmin);
+        $rolesUser = array();
+        array_push($rolesUser, 'ROLE_ADMIN');
+        $admin->setRoles($rolesUser);
+
+        $manager->persist($user);
+        $manager->persist($admin);
+
 
         $manager->flush();
     }
--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -97,9 +97,8 @@ class Authenticator extends AbstractFormLoginAuthenticator implements PasswordAu
         }
 
         return new RedirectResponse($this->urlGenerator->generate('about'));
-
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        throw new Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
